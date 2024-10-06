60e7e8fed22c8d6a8b8229a49b055856d211d033
sungyu0309 <tjsrbyt@gmail.com>
2024-10-06 19:14:34 +0900

    feat : add data model

--- src/components/User/CreateUser.js
+++ src/components/User/CreateUser.js
@@ -1,77 +1,137 @@
-import React, { useState } from 'react'
-import {useNavigate } from "react-router-dom";
-import Loader from '../Common/Loader';
-import './User.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Loader from "../Common/Loader";
+import "./User.css";
 const CreateUser = () => {
-    const navigate = useNavigate();
-    const createUserApi = "http://localhost:3000/user"
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [user, setUser] = useState({
-        name: "",
-        email: "",
-        phone: ""
-    })
+  const navigate = useNavigate();
+  const createUserApi =
+    "https://67025998bd7c8c1ccd3e9efc.mockapi.io/api/person";
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    gender: "",
+    birthDate: "",
+  });
 
-    const handelInput = (event) => {
-        event.preventDefault();
-        const { name, value } = event.target;
-        console.log(name, value)
-        setUser({ ...user, [name]: value });
+  const handelInput = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    console.log(name, value);
+    setUser({ ...user, [name]: value });
+  };
+
+  const handelSubmit = async (event) => {
+    event.preventDefault();
+    console.log(user);
+    try {
+      setIsLoading(true);
+      const response = await fetch(createUserApi, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+
+      if (response.ok) {
+        console.log("Form submitted successfully!");
+        setUser({ name: "", email: "", phone: "" });
+        navigate("/show-user");
+      } else {
+        console.error("Form submission failed!");
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    const handelSubmit = async (event) => {
-        event.preventDefault();
-        console.log(user)
-        try {
-            setIsLoading(true);
-            const response = await fetch(createUserApi, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(user),
-            });
-
-            if (response.ok) {
-                console.log('Form submitted successfully!');
-                setUser({name: "",email: "",phone: ""})
-                navigate('/show-user');
-            } else {
-                console.error('Form submission failed!');
-            }
-
-        } catch (error) {
-            setError(error.message);
-        } finally{
-            setIsLoading(false);
-        }
-    }
-
-    return (
-        <div className='user-form'>
-            <div className='heading'>
-            {isLoading && <Loader />}
-            {error && <p>Error: {error}</p>}
-                <p>User Form</p>
-            </div>
-            <form onSubmit={handelSubmit}>
-                <div className="mb-3">
-                    <label for="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" name="name" value={user.name} onChange={handelInput} />
-                </div>
-                <div className="mb-3 mt-3">
-                    <label for="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="email" name="email" value={user.email} onChange={handelInput} />
-                </div>
-                <div className="mb-3">
-                    <label for="pwd" className="form-label">Phone</label>
-                    <input type="text" className="form-control" id="phone" name="phone" value={user.phone} onChange={handelInput} />
-                </div>
-                <button type="submit" className="btn btn-primary submit-btn">Submit</button>
-            </form>
+  return (
+    <div className="user-form">
+      <div className="heading">
+        {isLoading && <Loader />}
+        {error && <p>Error: {error}</p>}
+        <p>User Form</p>
+      </div>
+      <form onSubmit={handelSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={user.name}
+            onChange={handelInput}
+            required
+          />
         </div>
-    )
-}
+        <div className="mb-3 mt-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={user.email}
+            onChange={handelInput}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="phone" className="form-label">
+            Phone
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="phone"
+            name="phone"
+            value={user.phone}
+            onChange={handelInput}
+            required
+          />
+        </div>
+        <div>
+          <label className="form-label">Gender</label>
+          <div className="wrapper">
+            <div>
+              <label htmlFor="man">Man</label>
+              <input type="radio" value="man" name="gender" id="man" checked />
+            </div>
+            <div>
+              <label htmlFor="woman">Woman</label>
+              <input type="radio" value="woman" name="gender" id="woman" />
+            </div>
+          </div>
+        </div>
+        <div className="vertical">
+          <label htmlFor="birthDate" className="form-label">
+            Birth Date
+          </label>
+          <input
+            type="date"
+            id="birthDate"
+            name="birthDate"
+            value={user.birthDate}
+            onChange={handelInput}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary submit-btn">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default CreateUser
+export default CreateUser;
